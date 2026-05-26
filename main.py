fa53e4f2c206c3924267f3771cb8ad535268ce1c
Daniel <daniellimas2000@gmail.com>
2026-05-26 16:51:24 -0300

    update: cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
 from routers import weather, export
 
@@ -8,6 +9,19 @@ app = FastAPI(
     title="Weather API - PM Accelerator",
     description="Backend API for Weather Application",
     version="1.0.0"
+)
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(weather.router)
